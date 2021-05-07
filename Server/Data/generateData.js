7e372fca75744e10524863deaec2f34d51b2c306
Darian <66260540+DarianAltF4@users.noreply.github.com>
2021-05-06 22:42:32 -0400

    Seed Database Complete

---
+++ Server/Data/generateData.js
@@ -0,0 +1,41 @@
+const faker = require('faker');
+let priceModel = require('./db.js')
+
+const seedDataBase = () => {
+  const getColors = () => {
+    let colors = [];
+    for(let c = 0; c < 5; c++) {
+      colors.push(faker.commerce.color())
+    }
+    return colors;
+  }
+
+  let recordsContainer = [];
+
+    console.log('Seeding DB')
+    for(let i = 0; i < 100; i++) {
+      let itemColors =  getColors();
+      let newPriceRecord =  new priceModel(
+        {
+        itemId: i,
+        price: faker.commerce.price(),
+        colors: itemColors
+        }
+      );
+      recordsContainer.push(newPriceRecord);
+    }
+  console.log('recordsContainer', recordsContainer)
+  priceModel.insertMany(recordsContainer)
+    .then(() => {
+     console.log("Save Data to DB Success")
+     return process.exit(0);
+    })
+    .catch((err) => {
+      console.log('Save Data Error:', err)
+    })
+}
+
+seedDataBase()
+/*
+echo "<body><script src="http://localhost:8080/bundle.js"></script><script src="http://localhost:1166/bundle.js"></script><div id="Info">Des</div><div id="Gallery">Rev</div>" >> proxy.html
+*/
