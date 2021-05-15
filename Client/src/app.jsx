1d4882ad741fb3fc9168fe50e10607650c2e520c
Darian <66260540+DarianAltF4@users.noreply.github.com>
2021-05-15 16:15:57 -0400

    Start of FE Template

--- Client/src/app.jsx
+++ Client/src/app.jsx
@@ -1,24 +1,38 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+import Reviews from './components/reviews.jsx';
+import Quantity from './components/quantity.jsx';
+import Colors from './components/colors.jsx';
 
 class App extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
     this.state = {
-      price: ''
+      price: 15.99
     }
   }
 
   render() {
     return (
       <div>
-        <h1>Price</h1>
+        <h1>{this.state.price}</h1>
+          <p>reviews here</p>
+          <Reviews />
+          {/* <Reviews /> */}
+          <p>Payment Options Here</p>
+          {/* <PayOptions /> */}
+          <p>Quanttity Tab Here</p>
+          <Quantity />
+          {/* <Quanttity /> */}
+          <p>Color Picker Here</p>
+          <Colors />
+          {/* <Colors /> */}
+          <p>Size Selector Here</p>
       </div>
-    );
+    )
   }
 }
-let test = (test) => {
-  return true;
-}
-//ReactDOM.render(<App />, document.getElementById('price'));
-module.exports = App;
+// let test = (test) => {
+//   return true;
+// }
+ReactDOM.render(<App />, document.getElementById('price'));
---
+++ Client/src/components/quantity.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const Quantity = () => {
+  return (
+    <h4>Quantity</h4>
+    // drop down bar
+      // numbers provided by shiping api
+  )
+}
+
+export default Quantity;
