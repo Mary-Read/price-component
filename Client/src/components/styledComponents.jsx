1114a4b9c7fc3cef73558f13636f75152189964b
Darian <66260540+DarianAltF4@users.noreply.github.com>
2021-05-27 23:59:59 -0400

    More CSS Updates

--- Client/src/components/styledComponents.jsx
+++ Client/src/components/styledComponents.jsx
@@ -280,6 +280,91 @@ const SpanTag = styled.span`
 
 `;
 
+const ButtonTab = styled.div`
+
+	/* Font & Text */
+	font-family: Targetica, "Helvetica Neue", Helvetica, Arial, sans-serif;
+	font-size: 16px;
+	font-style: normal;
+	font-variant: normal;
+	font-weight: 400;
+	letter-spacing: normal;
+	line-height: 22.856px;
+	text-decoration: none solid rgb(51, 51, 51);
+	text-align: start;
+	text-indent: 0px;
+	text-transform: none;
+	vertical-align: baseline;
+	white-space: normal;
+	word-spacing: 0px;
+
+	/* Color & Background */
+	background-attachment: scroll;
+	background-color: rgb(255, 255, 255);
+	background-image: none;
+	background-position: 0% 0%;
+	background-repeat: repeat;
+	color: rgb(51, 51, 51);
+
+	/* Box */
+	height: 357.938px;
+	width: 202px;
+	border: 1px solid rgb(51, 51, 51);
+	border-top: 1px solid rgb(51, 51, 51);
+	border-right: 1px solid rgb(51, 51, 51);
+	border-bottom: 1px solid rgb(51, 51, 51);
+	border-left: 1px solid rgb(51, 51, 51);
+	margin: 0px;
+	padding: 0px;
+	max-height: none;
+	min-height: 0px;
+	max-width: none;
+	min-width: 0px;
+
+	/* Positioning */
+	position: relative;
+	top: 0px;
+	bottom: 0px;
+	right: 0px;
+	left: 0px;
+	float: none;
+	display: block;
+	clear: none;
+	z-index: auto;
+
+	/* List */
+	list-style-image: none;
+	list-style-type: disc;
+	list-style-position: outside;
+
+	/* Table */
+	border-collapse: separate;
+	border-spacing: 0px 0px;
+	caption-side: top;
+	empty-cells: show;
+	table-layout: auto;
+
+	/* Miscellaneous */
+	overflow: visible;
+	cursor: auto;
+	visibility: visible;
+
+	/* Effects */
+	transform: none;
+	transition: all 0s ease 0s;
+	outline-offset: 0px;
+	box-sizing: border-box;
+	resize: none;
+	text-shadow: none;
+	text-overflow: clip;
+	word-wrap: break-word;
+	box-shadow: none;
+	border-top-left-radius: 4px;
+	border-top-right-radius: 4px;
+	border-bottom-left-radius: 4px;
+	border-bottom-right-radius: 4px;
+`
+
 const ButtonLabel = styled.div`
 font-weight: bold;
 transform: translate3d(0px, -12px, 0px);
@@ -389,5 +474,6 @@ export {
   DivTagPrice,
   SpanTag,
   ButtonTag,
-	ButtonLabel
+	ButtonLabel,
+	ButtonTab
 }
