0c6a99fea77e9cd100e994c8dc9de5ffd294fc09
Darian <66260540+DarianAltF4@users.noreply.github.com>
2021-06-11 22:54:31 -0400

    star random test

--- Client/src/components/reviews.jsx
+++ Client/src/components/reviews.jsx
@@ -13,7 +13,7 @@ class Reviews extends React.Component {
     let x = Math.floor(Math.random() * 5)
     for(let i = 0; i < x; i++) {
       return (
-        '⭐'️
+        <tr>'⭐'</tr>️
       )
     }
   }
@@ -23,7 +23,9 @@ class Reviews extends React.Component {
       <div>
         {/* temp place holders */}
         {/* ⭐️⭐️⭐️⭐️⭐️ */}
+        <table>
         {this.stars()}
+        </table>
         <span>233</span>
       </div>
     )
