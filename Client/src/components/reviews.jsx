efb2de2e52967c8e5931468ca1bf063ffb3d65a7
Darian <66260540+DarianAltF4@users.noreply.github.com>
2021-06-11 22:50:55 -0400

    stars change

--- Client/src/components/reviews.jsx
+++ Client/src/components/reviews.jsx
@@ -5,15 +5,25 @@ class Reviews extends React.Component {
     super(props);
     this.state = {
 
-    }
+    },
+    this.stars = this.stars.bind(this);
   }
 
+  stars() {
+    let x = Math.floor(Math.random() * 5)
+    for(let i = 0; i < x; i++) {
+      return (
+        ⭐️
+      )
+    }
+  }
 
   render() {
     return (
       <div>
         {/* temp place holders */}
-        ⭐️⭐️⭐️⭐️⭐️
+        {/* ⭐️⭐️⭐️⭐️⭐️ */}
+        {this.stars()}
         <span>233</span>
       </div>
     )
